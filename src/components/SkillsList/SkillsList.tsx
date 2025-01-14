6912143f33a8527be2772fb82379a3f2dde66b1d
Егор Кушаков <e.kyshakov@kosmosoft.ru>
2025-01-14 10:55:26 +0300

    feat: Добавить график уровня навыков

--- src/components/SkillsList/SkillsList.tsx
+++ src/components/SkillsList/SkillsList.tsx
@@ -17,6 +17,8 @@ import {
 } from 'react-icons/si';
 import { MdBuild } from 'react-icons/md';
 import { useSkillsContext } from '../SkillsContext';
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 
 const SkillsList = () => {
 	const hardSkills = [
@@ -24,34 +26,34 @@ const SkillsList = () => {
 			text: 'Фронтенд разработка',
 			icon: <MdBuild />,
 			items: [
-				{ name: 'HTML', icon: <FaHtml5 /> },
-				{ name: 'CSS', icon: <FaCss3Alt /> },
-				{ name: 'JavaScript', icon: <FaJs /> },
-				{ name: 'React', icon: <FaReact /> },
-				{ name: 'AS3', icon: <SiAsciidoctor /> }
+				{ name: 'HTML', icon: <FaHtml5 />, level: 70 },
+				{ name: 'CSS', icon: <FaCss3Alt />, level: 75 },
+				{ name: 'JavaScript', icon: <FaJs />, level: 95 },
+				{ name: 'React', icon: <FaReact />, level: 60 },
+				{ name: 'AS3', icon: <SiAsciidoctor />, level: 95 }
 			]
 		},
 		{
 			text: 'Контроль версий',
 			icon: <MdBuild />,
-			items: [{ name: 'Git', icon: <FaGitAlt /> }]
+			items: [{ name: 'Git', icon: <FaGitAlt />, level: 70 }]
 		},
 		{
 			text: 'Инструменты разработки',
 			icon: <MdBuild />,
 			items: [
-				{ name: 'Visual Studio Code', icon: <FaLaptopCode /> },
-				{ name: 'Sublime Text', icon: <FaLaptopCode /> }
+				{ name: 'Visual Studio Code', icon: <FaLaptopCode />, level: 95 },
+				{ name: 'Sublime Text', icon: <FaLaptopCode />, level: 95 }
 			]
 		},
 		{
 			text: 'Графический дизайн',
 			icon: <MdBuild />,
 			items: [
-				{ name: 'Corel Draw', icon: <SiCoreldraw /> },
-				{ name: 'Photoshop', icon: <SiAdobephotoshop /> },
-				{ name: 'Illustrator', icon: <SiAdobeillustrator /> },
-				{ name: 'Figma', icon: <SiFigma /> }
+				{ name: 'Corel Draw', icon: <SiCoreldraw />, level: 80 },
+				{ name: 'Photoshop', icon: <SiAdobephotoshop />, level: 60 },
+				{ name: 'Illustrator', icon: <SiAdobeillustrator />, level: 60 },
+				{ name: 'Figma', icon: <SiFigma />, level: 30 }
 			]
 		},
 		{
@@ -60,7 +62,8 @@ const SkillsList = () => {
 			items: [
 				{
 					name: 'Активный пользователь различных ИИ для генерации текста, рисунков',
-					icon: <FaNetworkWired />
+					icon: <FaNetworkWired />,
+					level: 70
 				}
 			]
 		}
@@ -97,6 +100,16 @@ const SkillsList = () => {
 										<div key={idx} className="skill-block">
 											<span className="skill-icon">{item.icon}</span>
 											<span>{item.name}</span>
+											<div className="skill-chart">
+												<CircularProgressbar
+													value={item.level}
+													strokeWidth={50}
+													className="custom-progressbar"
+													styles={buildStyles({
+														strokeLinecap: 'butt'
+													})}
+												/>
+											</div>
 										</div>
 									))}
 								</div>
